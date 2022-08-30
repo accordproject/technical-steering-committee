92f14649133b2f7dc31f37dd19ea9fe60e77369f
Dan Selman <danscode@selman.org>
2022-08-30 13:31:54 +0100

    chore(ignore) remove build output
    Signed-off-by: Dan Selman <danscode@selman.org>

--- Tools/contributors/built/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var gitlog_1 = require("gitlog");
-var result = (0, gitlog_1.default)({
-    repo: "foo",
-    fields: ["subject", "authorName", "authorDate"],
-});
-console.log(result);
