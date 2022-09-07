42e954d67dadd9c2b762541ed2bbbba4db3f37ea
Matt Roberts <Matt.Roberts@docusign.com>
2022-09-07 14:47:27 +0100

    feat(tools): list recent commits
    Signed-off-by: Matt Roberts <Matt.Roberts@docusign.com>

--- Tools/contributors/src/index.ts
+++ Tools/contributors/src/index.ts
@@ -24,6 +24,16 @@ async function getRepoContributors(repo: string) {
   return await response.json();
 }
 
+async function getRepoCommits(repo: any, since: string) {
+  const response = await fetch(`https://api.github.com/repos/accordproject/${repo.name}/commits?since=${since}`,  {
+    method: "GET",
+    headers: {
+      Authorization: `token ${process.env['GITHUB_PAT']}`
+    }
+  });
+  return [repo, await response.json()];
+}
+
 /**
  * Write the contributors to all Accord Project repositories
  * in Helios Voting fomat to a CSV file:
@@ -42,4 +52,18 @@ async function writeContributorsToCsv() {
   console.log('Created file contributors.csv.');
 }
 
+/**
+ * Write a CSV file listing all merged commits in the last week
+ */
+ async function writeRecentCommitsToCsv() {
+  const repos = await getRepos();
+  const lastWeek = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+  const promises = repos.map(repo => getRepoCommits(repo, lastWeek.toISOString()));
+  const results = await Promise.all(promises);
+  const names = results.flatMap(([repo, commits]) => commits.map(c => `${repo.full_name},${c.commit.message.split('\n')[0]},${c.author.login},${c.html_url}`));
+  fs.writeFileSync("recentCommits.csv", names.join('\n'));
+  console.log('Created file recentCommits.csv.');
+}
+
+writeRecentCommitsToCsv();
 writeContributorsToCsv();
